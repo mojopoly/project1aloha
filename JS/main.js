fd105927be07d1b11cbf5b20ac67f506c473d4f6
mojopoly <cyrus.eslamian@gmail.com>
2019-01-21 10:07:16 -0500

    80% done for project 1

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+
+$(function() {
+    // your jQuery code here...
+  $(document).ready(function(){
+    $('.mostLovedProds-below').flickity({
+      cellAlign: 'left',
+      wrapAround: true
+    });
+  });    
+  });
+
+  //no need to setup width for mobile-first
+  //commit at least 5 times
+  //include slider as plugin in your Read more
+  //make code more readable, add sections
+  //use smooth scrolling function in css
+  //field isnt empty and value is an email for alert boxx
+  //stretch goals: use jQuery for sticky menu
